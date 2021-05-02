e98eb4c094d380f1b31c641bed870c0d5d1165e2
thothdeus <fagundescf35@gmail.com>
2021-05-02 13:39:37 -0300

    MayApi

---
+++ src/consultas.js
@@ -0,0 +1,22 @@
+const consultas = (prefix, pushname) => {
+	return `「 *CONSULTAS* 」
+「 EM CRIAÇÃO
+  ❏ cnpj (123456)
+  ❏ map (lugar)
+  ❏ ip (192.2.34)」
+
+「 GERADORES
+  ❏ cpf1 (sem flood)
+  ❏ cnpj1 (sem flood)
+  ❏ cns1 (sem flood)
+  ❏ cnh1 (sem flood)
+  ❏ pis1 (sem flood)
+  ❏ cc1 (sem flood)
+  ❏ cc2 (sem flood)
+  ❏ pessoa (sem flood) 」
+
+「 IMPORTANTE: A má utilização dos dados aqui gerados é de total responsabilidade do usuário.
+   Os números são gerados de forma aleatória, respeitando as regras de criação de cada documento. 」`
+}
+
+exports.consultas = consultas
---
+++ src/entretenimento.js
@@ -0,0 +1,33 @@
+const entretenimento = (prefix) => {
+	return `
+「 *ENTRETENIMENTO*
+     SEM FLOOD 」
+   ❏ ${prefix}roleta
+   ❏ ${prefix}game
+   ❏ ${prefix}buscar
+   ❏ ${prefix}kill
+   ❏ ${prefix}tiktok
+   ❏ ${prefix}tapa
+   ❏ ${prefix}xhamster
+   ❏ ${prefix}livro
+   ❏ ${prefix}ytplay
+   ❏ ${prefix}fbdl
+   ❏ ${prefix}letra
+   ❏ ${prefix}twtdl
+   ❏ ${prefix}freefire
+   ❏ ${prefix}cobrar
+   ❏ ${prefix}hentai
+   ❏ ${prefix}blowjob
+   ❏ ${prefix}happymod
+   ❏ ${prefix}kiss
+   ❏ ${prefix}may (texto)
+   ❏ ${prefix}cry
+   ❏ ${prefix}loli
+   ❏ ${prefix}lyric (tipografia)
+   ❏ ${prefix}cassino
+   ❏ ${prefix}pedra
+   ❏ ${prefix}papel
+   ❏ ${prefix}tesoura`
+}
+
+exports.entretenimento = entretenimento
