bde0f3620e62c1a0ffb549da8446bc45e62bfd8e
lakisan <42392381+lakisan@users.noreply.github.com>
2021-05-13 10:52:50 -0200

    Update index.js

--- index.js
+++ index.js
@@ -499,8 +499,8 @@ break
 					case 'shipp':		
 	            	if (args.length < 1) return reply('marque o casal!')
 					rate = body.slice(1)
-					const ti =['5','10','18','29','38','44*','60','63','75','84','98','101','30','95','76','83','45','34']
-					const kl = ti[Math.floor(Math.random() * ti.length)]
+					const lost =['5','10','18','29','38','44*','60','63','75','84','98','101','30','95','76','83','45','34']
+					const kl = lost[Math.floor(Math.random() * ti.length)]
 					thoth.sendMessage(from, 'Olha o casalzin: *'+rate+'*\n\nA chance de dar certo é : '+ kl+'%', text, { quoted: mek })
 					break
                 case 'ytplay':
