301b2901d2e6749b61640882ab0e801bb5b5436e
lakisan <42392381+lakisan@users.noreply.github.com>
2021-05-14 20:29:50 -0200

    Update and rename novela to novela.js

---
+++ src/novela.js
@@ -0,0 +1,9 @@
+const novela = (prefix, sender) => {
+	return `
+「 Novelas em pdf
+  ❏ ${prefix}lvl1 (Moby Dick - Herman Melville)
+  ❏ ${prefix}lvl2 (Kamasutra - Alicia Galloti)
+  ❏ ${prefix}lvl3 (O Alquimista - Paulo Coelho)
+}
+
+exports.novela = novela
