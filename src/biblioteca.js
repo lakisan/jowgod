80a36870b40fba9ef9e62347619f42c9a33a14b6
lakisan <42392381+lakisan@users.noreply.github.com>
2021-05-15 12:27:03 -0200

    Update biblioteca.js

--- src/biblioteca.js
+++ src/biblioteca.js
@@ -1,16 +1,11 @@
 const biblioteca = (prefix, sender) => {
 	return `
 「 BIBLIOTECA 
-   ❏ ${prefix}lv1 (Mitologia Nórdica)   
-   ❏ ${prefix}lv2 (Eu tenho sérios poemas mentais)
-   ❏ ${prefix}lv3 (O fim em doses homeopáticas)
-   ❏ ${prefix}lv4 (Lugar Nenhum)
-   ❏ ${prefix}lv5 (A Menina que Roubava Livros)
-   ❏ ${prefix}lv6 (O Segredo)
-   ❏ ${prefix}lv7 (Esmeralda)
-   ❏ ${prefix}lv8 (O Pequeno Príncipe)
-   ❏ ${prefix}lv9 (O Alquimista)
-   ❏ ${prefix}lv10 (Coraline) 」`
+   ❏ ${prefix}horror
+   ❏ ${prefix}poesia
+   ❏ ${prefix}suspense
+   ❏ ${prefix}novelas
+   ❏ ${prefix}romance 」`
 }
 
 exports.biblioteca = biblioteca
