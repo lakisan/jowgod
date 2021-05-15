a148bfee877fc46a6dcc07eb85de689c23c9ed70
lakisan <42392381+lakisan@users.noreply.github.com>
2021-05-15 12:16:01 -0200

    Update novela.js

--- src/novela.js
+++ src/novela.js
@@ -2,9 +2,9 @@ const novela = (prefix, pushname) => {
 	return `「 *Livros* 」
 「 Novelas
    ❏ ${prefix}lv1 (Moby Dick - Herman Melville)   
-   ❏ ${prefix}lv1 (O Alquimista - Paulo Coelho)  
-   ❏ ${prefix}lv2 (Kamasutra - Alicia Galotti)
-   ❏ ${prefix}lv2 (O Código Da Vinci)`
+   ❏ ${prefix}lv2 (O Alquimista - Paulo Coelho)  
+   ❏ ${prefix}lv3 (Kamasutra - Alicia Galotti)
+   ❏ ${prefix}lv4 (O Código Da Vinci)`
 }
 
 exports.novela = novela
