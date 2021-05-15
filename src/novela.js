9ddfad2970c9239af3562c38b77b568c706a15a8
lakisan <42392381+lakisan@users.noreply.github.com>
2021-05-14 23:09:11 -0200

    Update novela.js

--- src/novela.js
+++ src/novela.js
@@ -2,6 +2,6 @@ const novela = (prefix, pushname) => {
 	return `「 *Livros* 」
 「 Novelas
    ❏ ${prefix}lv1 (Mitologia Nórdica)   
-   ❏ ${prefix}lv2 (Eu tenho sérios poemas mentais)
+   ❏ ${prefix}lv2 (Eu tenho sérios poemas mentais)`
 
 exports.novela = novela
