220b5ab25b35bad62567b4431e60703f108fb688
lakisan <42392381+lakisan@users.noreply.github.com>
2021-05-16 17:16:51 -0300

    Create ficcao.js

---
+++ src/ficcao.js
@@ -0,0 +1,12 @@
+const ficcao = (prefix, pushname) => {
+	return `「 *Livros* 」
+「 Ficção
+
+   ❏ ${prefix}lv13 (Da Terra a Lua)
+   ❏ ${prefix}lv14 (A Guerra dos Mundos)
+   ❏ ${prefix}lv15 (Vinte Mil Léguas Submarinas)
+   ❏ ${prefix}lv16 (Até o Último Homem)
+   ❏ ${prefix}lv17 (a Máquina do Tempo
+}
+
+exports.ficcao = ficcao 
